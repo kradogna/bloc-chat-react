ff74323a81ab1af51fa97f93ffccf53515c8ce92
unknown <kradogna99@gmail.com>
2018-08-09 14:10:37 -0700

    Added firebase and room list - not working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase';
+import { RoomList } from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCFGxdKhFyvHx9EAB_IQQuSejx9KEUQCu4",
+  authDomain: "bloc-chat-react-b6ddb.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-b6ddb.firebaseio.com",
+  projectId: "bloc-chat-react-b6ddb",
+  storageBucket: "bloc-chat-react-b6ddb.appspot.com",
+  messagingSenderId: "557531276027"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase}/>
       </div>
     );
   }
